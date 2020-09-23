6234b6aafa2784b040a502c1231edf79d504b0f1
Imad Razzak <im.razzak@gmail.com>
2020-09-23 19:03:05 +0200

    move types

--- src/data/campaigns/index.ts
+++ src/data/campaigns/index.ts
@@ -1,6 +1,17 @@
-import type { Campaign } from '../../types/Campaign';
 import mortal from './mortal';
 import vortex from './vortex';
+
+type Campaign = {
+  readonly key: string;
+  readonly name: string;
+  readonly map: {
+    readonly image: string;
+    readonly imageText: string;
+    readonly width: number;
+    readonly height: number;
+  };
+  readonly regions: any;
+};
 
 export default {
   mortal,
